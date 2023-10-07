967f88ada990db4dea56588cb26572e667594d4b
mohaned5 <mohaned@live.co.uk>
2023-10-08 00:50:15 +0100

    completed challenge

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+from collections import Counter
+
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    string_list = [str(item) for item in items]
+    count_items = Counter(string_list)
+    for key in count_items.keys():
+        print(str(key) + ":" + str(count_items[key]))
+        frequencies[key] = count_items[key]
     return frequencies
